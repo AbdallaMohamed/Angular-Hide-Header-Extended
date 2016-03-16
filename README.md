0bc405e0d7e77410e430744be7f8a11a6a751f6f
Abdalla Mohamed El-Abd <abdalla.mohamed.elabd@gmail.com>
2016-03-16 22:08:34 +0200

    Initial check-in

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# Angular-Hide-Header-Extended
-Angular directive for hiding a header when scrolling down and showing it when scrolling up
+Angular Hide Header Extended
+============================
+
+Angular directive to hide or show header when user scroll down or up.
+Could use on infinite scroll pages.
+
+##### This is an extended version of Mario Flores's directive: https://github.com/pokaxperia/angular-hide-header
+
+Demo
+-------
+Check out [the live demo](http://run.plnkr.co/plunks/ePnxK2).
+
+Installation
+-------
+
+#### via bower:
+
+```
+$ bower install angular-hide-header-extended --save
+```
+
+Usage
+---
+
+1. Include **angularHideHeaderExtended** as a dependency for your app
+
+  ```
+  angular.module('myApp', ['angularHideHeaderExtended'])
+  ```
+  
+2. Easy, add the directive to header element you want to hide or show.
+
+    ```
+    <header hide-header hide-header-on="true">
+    </header>
+    ```
+     This is the most basic example.
+  
+Configuration
+--- 
+*  Add **hide-offset** to specify from which pixels from the top header will hide. Default value is 60.
+
+    hide-offset="80"  
+     
+     ```
+      <header hide-header hide-offset="80">
+      </header>
+      ```
+
+*  Add **hide-header-on** to bind to a scope variable that controls if the header is to be hidden or not.
+
+    hide-header-on="true" // default behaviour
+    hide-header-on="hideHeader" // default behaviour   
+    
+    ```
+  <header hide-header hide-header-on="hideHeader">
+  ```
+  ```
+  Hide Header? <input type="checkbox" ng-model="hideHeader" />
+  ```
+  ```
+  </header>
+  ```
+
+---------
---
+++ dist/angular-hide-header-extended.js
@@ -0,0 +1,45 @@
+(function(){
+	'use strict';
+	angular.module('angularHideHeaderExtended',[]).directive('hideHeader',['$timeout','$window',function($timeout,$window){
+		return{
+			restrict:'A',
+            scope: {
+                hideHeaderOn: '='
+            },
+		  	link: function (scope, element, attrs) {
+				var scrollposition = 0,scroll_time;
+				var hideOffset = attrs.hideOffset;
+                scope.$watch('hideHeaderOn', function (val) {
+                    if(val == true) {
+                        angular.element($window).bind("scroll", function() {
+                            var body = angular.element(document.getElementsByTagName('body'));
+                            var current_scroll = body[0].scrollTop;
+                            var hheight = element[0].scrollHeight;
+                            var pxOffset = parseInt(hideOffset);
+                            $timeout.cancel(scroll_time);
+                            if(current_scroll >= hheight+pxOffset){
+                                if (current_scroll < scrollposition) {
+                                    element.css({'top':"0px"});
+                                }
+                                else if (current_scroll > scrollposition) {
+                                    element.css({
+                                        'top':-hheight+"px",
+                                        'transition':'all 0.25s',
+                                        '-webkit-transition': 'all 0.25s',
+                                        '-moz-transition': 'all 0.25s',
+                                        '-ms-transition': 'all 0.25s',
+                                        '-o-transition': 'all 0.25s',
+                                    });
+                                }
+                            }
+                            scroll_time = $timeout(function(){
+                                scrollposition = body[0].scrollTop;
+                            },60);
+                        });
+                    }
+                });
+
+			}
+		}
+	}]);
+}());
